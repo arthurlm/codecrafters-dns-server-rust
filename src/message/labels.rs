f77573c2da6b91d6495e4477814cc092fbef9ba5
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-10 10:13:39 +0100

    Make label parser a lot more simple

--- src/message/labels.rs
+++ src/message/labels.rs
@@ -2,12 +2,12 @@ use nom::{
     bits::{bits, complete::take},
     branch::alt,
     bytes::complete::tag,
-    combinator::{value, verify},
-    error::{Error, ErrorKind},
+    combinator::{success, value, verify},
+    error::Error,
     multi::{length_data, many_till},
     number::complete::be_u8,
     sequence::tuple,
-    Err, IResult, Parser,
+    IResult, Parser,
 };
 
 /// Parse label output.
@@ -16,42 +16,33 @@ use nom::{
 pub type ParseLabelOutput = (Vec<String>, Option<u16>);
 
 pub fn parse(input: &[u8]) -> IResult<&[u8], ParseLabelOutput> {
-    alt((parse_regular, parse_compressed))(input)
-}
-
-fn parse_regular(input: &[u8]) -> IResult<&[u8], ParseLabelOutput> {
-    match many_till(
-        // Map each "segment" into a string.
-        length_data(be_u8).map(data_to_string),
-        // Until = EOT.
-        tag("\0"),
-    )(input)
-    // Convert error as recoverable error so alt we try to parse the data.
-    {
-        Ok((input, (names, _))) => Ok((input, (names, None))),
-        Err(Err::Failure(e) | Err::Error(e)) => Err(Err::Error(e)),
-        Err(Err::Incomplete(_)) => Err(Err::Error(Error::new(input, ErrorKind::Alt))),
-    }
-}
-
-fn parse_compressed(input: &[u8]) -> IResult<&[u8], ParseLabelOutput> {
     let (input, (names, (_, offset))) = many_till(
         // Map each "segment" into a string.
         length_data(be_u8).map(data_to_string),
-        // until = split 16 next bytes into 2 + 14 bits.
-        bits(tuple((
-            // Discard first 2 bits.
-            value(
-                (),
-                // Check if first 2 bits are 0b11.
-                verify(take::<_, u8, _, Error<_>>(2_usize), |v| *v == 0b11),
-            ),
-            // Keep remaining bits as "offset".
-            take::<_, u16, _, Error<_>>(14_usize),
-        ))),
+        // Until:
+        alt((
+            // EOT.
+            tuple((
+                // Complete with empty value.
+                success(()),
+                // Return None as there is no next offset.
+                value(None, tag("\0")),
+            )),
+            // Or split 16 next bytes into 2 + 14 bits.
+            bits(tuple((
+                // Discard first 2 bits.
+                value(
+                    (),
+                    // Check if first 2 bits are 0b11.
+                    verify(take::<_, u8, _, Error<_>>(2_usize), |v| *v == 0b11),
+                ),
+                // Keep remaining bits as "offset".
+                take(14_usize).map(Some),
+            ))),
+        )),
     )(input)?;
 
-    Ok((input, (names, Some(offset))))
+    Ok((input, (names, offset)))
 }
 
 fn data_to_string(input: &[u8]) -> String {
